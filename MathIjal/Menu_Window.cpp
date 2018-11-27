2204374fca55878336027bf9de9a87a8b771b810
kholishotula <37019775+kholishotula@users.noreply.github.com>
2018-11-27 13:32:03 +0700

    Update Menu_Window.cpp

--- MathIjal/Menu_Window.cpp
+++ MathIjal/Menu_Window.cpp
@@ -3,7 +3,7 @@
 #include <wx/filename.h>
 
 BEGIN_EVENT_TABLE(Menu_Window, wxWindow)
-EVT_PAINT(Menu_Window::Draw)
+	EVT_PAINT(Menu_Window::Draw)
 END_EVENT_TABLE();
 
 Menu_Window::Menu_Window(wxFrame *parent)
@@ -11,23 +11,39 @@ Menu_Window::Menu_Window(wxFrame *parent)
 {
 	wxImageHandler *pngLoader = new wxPNGHandler();
 	wxImage::AddHandler(pngLoader);
-	this->LoadMenuBitmap();
-}
 
-void Menu_Window::LoadMenuBitmap()
-{
-	wxImage image(wxT("\menu.png"), wxBITMAP_TYPE_PNG);
+	//Load Bitmap
+	wxImage image_menu(wxT("\menu.png"), wxBITMAP_TYPE_PNG);
+	menuBitmap = new wxBitmap(image_menu);
 
-	menuBitmap = new wxBitmap(image);
+	wxImage image_mulai(wxT("\mulai_text.png"), wxBITMAP_TYPE_PNG);
+	mulaiBitmap = new wxBitmap(image_mulai);
+
+	wxImage image_cara(wxT("\cara_text.png"), wxBITMAP_TYPE_PNG);
+	caraBitmap = new wxBitmap(image_cara);
+
+	wxImage image_pengaturan(wxT("\pengaturan_text.png"), wxBITMAP_TYPE_PNG);
+	pengaturanBitmap = new wxBitmap(image_pengaturan);
+
+	wxImage image_keluar(wxT("\keluar_text.png"), wxBITMAP_TYPE_PNG);
+	keluarBitmap = new wxBitmap(image_keluar);
 }
 
 void Menu_Window::Draw(wxPaintEvent &event)
 {
 	wxPaintDC pdc(this);
-	pdc.DrawBitmap(*menuBitmap, wxPoint(0, 0), true);
+	pdc.DrawBitmap(*menuBitmap, wxPoint(0, -5), true);
+	pdc.DrawBitmap(*mulaiBitmap, wxPoint(277, 250), true);
+	pdc.DrawBitmap(*caraBitmap, wxPoint(277, 350), true);
+	pdc.DrawBitmap(*pengaturanBitmap, wxPoint(277, 450), true);
+	pdc.DrawBitmap(*keluarBitmap, wxPoint(277, 550), true);
 }
 
 Menu_Window::~Menu_Window()
 {
 	delete menuBitmap;
+	delete mulaiBitmap;
+	delete caraBitmap;
+	delete pengaturanBitmap;
+	delete keluarBitmap;
 }
